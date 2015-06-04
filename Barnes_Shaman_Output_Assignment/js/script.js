2f34bba7ba654a4eca37241a86baff2ec9fef88d
Shaman Barnes <shamanbarnes1@me.com>
2015-06-03 21:42:44 -0400

    adding the age of the kids

--- Barnes_Shaman_Output_Assignment/js/script.js
+++ Barnes_Shaman_Output_Assignment/js/script.js
@@ -5,7 +5,8 @@
 var firstChild = 15; // age of my first born
 var secondChild = 11; // age of my second born
 var thirdChild = 2; // age of my third child 
-var answer;
+var question = "What\'s the total age id my kids?\n"; // The question before the addition
+var answer; // answer for all the age combine for my kids 
 
 answer = firstChild + secondChild + thirdChild;
 console.log(answer);
