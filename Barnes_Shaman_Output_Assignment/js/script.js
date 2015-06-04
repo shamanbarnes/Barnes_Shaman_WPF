c65214793333ca49c798ef9a6cfc78a31f20d903
Shaman Barnes <shamanbarnes1@me.com>
2015-06-03 21:39:42 -0400

    Adding comment to my code

--- Barnes_Shaman_Output_Assignment/js/script.js
+++ Barnes_Shaman_Output_Assignment/js/script.js
@@ -5,3 +5,7 @@
 var firstChild = 15; // age of my first born
 var secondChild = 11; // age of my second born
 var thirdChild = 2; // age of my third child 
+var answer;
+
+answer = firstChild + secondChild + thirdChild;
+console.log(answer);
