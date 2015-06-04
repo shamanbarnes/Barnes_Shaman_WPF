9f8b89c71f89dd9cf1c1b31e37a3fa119e40ab51
Shaman Barnes <shamanbarnes1@me.com>
2015-06-03 22:14:25 -0400

    assigning boolean value

--- Barnes_Shaman_Output_Assignment/js/script.js
+++ Barnes_Shaman_Output_Assignment/js/script.js
@@ -8,5 +8,6 @@ var thirdChild = 2; // age of my third child
 var question = "What\'s the total age id my kids?\n"; // The question before the addition
 var answer; // answer for all the age combine for my kids 
 
-answer = firstChild + secondChild + thirdChild;
-console.log(question + answer);
+answer = firstChild + secondChild + thirdChild; // calculate total kids age
+console.log(question + answer); // answer output 
+console.log( thirdChild > firstChild); //Assigning a Boolean value
