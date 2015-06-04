b3ead922b5271bb05b18691ec7a6cbcbdd9b47a8
Shaman Barnes <shamanbarnes1@me.com>
2015-06-03 20:12:37 -0400

    listing the amount of kids

--- Barnes_Shaman_Output_Assignment/js/script.js
+++ Barnes_Shaman_Output_Assignment/js/script.js
@@ -1,1 +1,3 @@
-console.log("Welcome to SDI");
+var firstName = "Shaman"; // firstName represents Shaman 
+var lastName = "Barnes"; // lastName represents Barnes 
+var myKids = ["Raquiva", "Jonelle", "Nijah"]; //Amount of kids by name 
