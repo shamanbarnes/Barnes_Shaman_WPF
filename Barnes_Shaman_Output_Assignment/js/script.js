e0572441010308f33c3d1c1a47426c043923a9c1
Shaman Barnes <shamanbarnes1@me.com>
2015-06-03 20:42:42 -0400

    Name and assignment

--- Barnes_Shaman_Output_Assignment/js/script.js
+++ Barnes_Shaman_Output_Assignment/js/script.js
@@ -1,3 +1,3 @@
-var firstName = "Shaman"; // firstName represents Shaman 
-var lastName = "Barnes"; // lastName represents Barnes 
-var myKids = ["Raquiva", "Jonelle", "Nijah"]; //Amount of kids by name 
+// Name: Shaman Barnes 
+// Class: Scalable Data Infrastructures
+// Assignment: Creating Variables & Output
