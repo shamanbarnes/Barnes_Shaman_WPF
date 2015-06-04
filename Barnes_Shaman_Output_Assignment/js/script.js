bd9bc406a10d73eb8bc4ba6e029004d5fad6385c
Shaman Barnes <shamanbarnes1@me.com>
2015-06-03 20:56:03 -0400

    Age of kids

--- Barnes_Shaman_Output_Assignment/js/script.js
+++ Barnes_Shaman_Output_Assignment/js/script.js
@@ -1,3 +1,7 @@
 // Name: Shaman Barnes 
 // Class: Scalable Data Infrastructures
 // Assignment: Creating Variables & Output
+
+var firstChild = 15; // age of my first born
+var secondChild = 11; // age of my second born
+var thirdChild = 2; // age of my third child 
