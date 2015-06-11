16d62395f3c399564b04982397ef255fedcc660f
Shaman Barnes <shamanbarnes1@me.com>
2015-06-10 21:20:33 -0400

    variables
    variables added

---
+++ Barnes_Shaman_Expression_Assignment/js/script.js
@@ -0,0 +1,13 @@
+// Name: Shaman Barnes 
+// Class: Scalable Data Infrastructures
+// Assignment: Expressions
+
+
+
+//variables 
+var yourHeight;
+var yourWeight;
+var years;
+var digits;
+var output = "Your BMI is";
+var results
