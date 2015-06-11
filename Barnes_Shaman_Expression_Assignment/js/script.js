4504bc01129be55c52aa5b0e579de8f4d505c573
Shaman Barnes <shamanbarnes1@me.com>
2015-06-10 21:23:30 -0400

    adding prompts

--- Barnes_Shaman_Expression_Assignment/js/script.js
+++ Barnes_Shaman_Expression_Assignment/js/script.js
@@ -15,3 +15,9 @@ var results
 
 // Listing an array of year dates
 var digits = [1970, 1980, 1990, 2000, 2010]; 
+var years  =  digits[0] > digits[3];
+
+
+//prompts
+yourName = prompt("Enter your name?");
+yourSex  = prompt("Male or Female?");
