b13419555461c4d834e916039b198453388b8810
Shaman Barnes <shamanbarnes1@me.com>
2015-06-10 21:25:13 -0400

    casting the prompts

--- Barnes_Shaman_Expression_Assignment/js/script.js
+++ Barnes_Shaman_Expression_Assignment/js/script.js
@@ -13,7 +13,7 @@ var output = "Your BMI is";
 var results
 
 
-// Listing an array of year dates
+//listing an array of year dates
 var digits = [1970, 1980, 1990, 2000, 2010]; 
 var years  =  digits[0] > digits[3];
 
@@ -21,3 +21,7 @@ var years  =  digits[0] > digits[3];
 //prompts
 yourName = prompt("Enter your name?");
 yourSex  = prompt("Male or Female?");
+
+//add casting to the prompts 
+yourHeight = Number(prompt("Enter your height in inches:"));
+yourWeight = Number(prompt("Enter your weight:"));
