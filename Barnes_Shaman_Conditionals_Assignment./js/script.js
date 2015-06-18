775f7ac9e0f2b8246f9f2301b9f34231ed03c768
Shaman Barnes <shamanbarnes1@me.com>
2015-06-18 11:27:56 -0400

    user should enter correct information

--- Barnes_Shaman_Conditionals_Assignment./js/script.js
+++ Barnes_Shaman_Conditionals_Assignment./js/script.js
@@ -5,3 +5,9 @@
 var loanAmount 		= Number(prompt("Please enter your loan amount")); // User should enter there loan amount
 var numberOfMonths 	= Number(prompt("Enter the amount of months")); // User should enter the amount of months for loan
 var income			= Number(prompt("Enter your monthly income")); //  User should enter their monthly income
+
+if (loanAmount <= 0 || isNaN(Number(loanAmount)) ); // if loan amount is not a number or less than 0
+{
+    var loanAmount = prompt("Please enter a valid loan amount");// prompt enter valid amount 
+}
+
