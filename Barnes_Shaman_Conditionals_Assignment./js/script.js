03b3ffe32e228b2b2c63bd4a2fd5aa470766b033
Shaman Barnes <shamanbarnes1@me.com>
2015-06-18 11:24:15 -0400

    var for users to enter there information

--- Barnes_Shaman_Conditionals_Assignment./js/script.js
+++ Barnes_Shaman_Conditionals_Assignment./js/script.js
@@ -2,3 +2,6 @@
 // Class: Scalable Data Infrastructures
 // Assignment: Conditionals
 
+var loanAmount 		= Number(prompt("Please enter your loan amount")); // User should enter there loan amount
+var numberOfMonths 	= Number(prompt("Enter the amount of months")); // User should enter the amount of months for loan
+var income			= Number(prompt("Enter your monthly income")); //  User should enter their monthly income
