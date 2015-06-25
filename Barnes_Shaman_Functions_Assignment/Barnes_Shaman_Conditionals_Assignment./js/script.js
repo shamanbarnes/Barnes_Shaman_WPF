378c8de0e64695ea5ef5ecfab2c3cdccaeec78f6
Shaman Barnes <shamanbarnes1@me.com>
2015-06-25 16:11:44 -0400

    Assignment for the function created

--- Barnes_Shaman_Functions_Assignment/Barnes_Shaman_Conditionals_Assignment./js/script.js
+++ Barnes_Shaman_Functions_Assignment/Barnes_Shaman_Conditionals_Assignment./js/script.js
@@ -4,5 +4,16 @@
 
 
 
-function genRanPowerballNum(min, max, num)
+function genRanPowerballNum(min, max, num) {
+
+    var powerballArray = [];
+
+    for (var i = 0; i < num; i++) {
+
+    	var ranNum = Math.random() * (max - min) + min;
+
+        while (powerballArray.indexOf(Math.round(ranNum)) > -1) {
+            ranNum = Math.random() * (max - min) + min;
+
+        }
 
