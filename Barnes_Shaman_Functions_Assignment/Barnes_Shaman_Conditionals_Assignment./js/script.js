0250608d20b8b7c9dc275d4c95a2d252986bcfd2
Shaman Barnes <shamanbarnes1@me.com>
2015-06-25 16:47:23 -0400

    comment added

--- Barnes_Shaman_Functions_Assignment/Barnes_Shaman_Conditionals_Assignment./js/script.js
+++ Barnes_Shaman_Functions_Assignment/Barnes_Shaman_Conditionals_Assignment./js/script.js
@@ -15,27 +15,28 @@ function genRanPowerballNum(min, max, num) {    //This function that generates t
 
     for (var i = 0; i < num; i++) {
 
-    	var ranNum = Math.random() * (max - min) + min;
+    	var ranNum = Math.random() * (max - min) + min;  //Math.random generates random numbers for our lottery game
 
-        while (powerballArray.indexOf(Math.round(ranNum)) > -1) {
+        while (powerballArray.indexOf(Math.round(ranNum)) > -1) { 
             ranNum = Math.random() * (max - min) + min;
 
         }
         powerballArray[i] = Math.round(ranNum);
 
     }
-    return powerballArray;
+    return powerballArray; //Calling out the var powerball Array
    }
-var numberOfTickets = Number(prompt("Enter the amount of tickets you would like to purchase. "));
+var numberOfTickets = Number(prompt("Enter the amount of tickets you would like to purchase. ")); //Asking the user to enter number of tickets 
 
 for (var i = 0; i < numberOfTickets; i++)
 {
     var tickects = genRanPowerballNum(1, 36, 6);
-    console.log("Here is your lottery tickets "   + (i+1) + " - " + tickects  +   " good luck! ");
+    console.log("Here is your lottery tickets "   + (i+1) + " - " + tickects  +   " good luck! "); //Printing out lottery numbers  
 }
 
-powerballLotteryRandom = genRanPowerballNum(1, 36, 5);    // 
+powerballLotteryRandom = genRanPowerballNum(1, 36, 5);    //Generating out lotto numbers form the return in the function 
 
 powerballRandom = genRanPowerballNum(1, 35, 1);
 
 console.log("Your Powerball lottery numbers are " + powerballLotteryRandom + ", and your Powerball number is " + powerballRandom + " good luck! ");
+//printing out our powerball numbers 
